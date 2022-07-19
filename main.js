a73f566f568c08c590b13023a577c0e0df4990ff
limjj88 <limjj88@naver.com>
2022-07-20 01:28:25 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const btnCall = document.querySelector(".btnCall");
+/*const menuMo = document.querySelector("menuMo");*/ 
+
+
+console.log(btnCall);
+
+
+const wrap =document.querySelector(".wrap");
+console.log(wrap)
+
+btnCall.onclick =function(e)
+{
+    e.preventDefault();
+
+    btnCall.classList.toggle("on");
+    /*menuMo.classList.toggle("on");*/ 
+}
